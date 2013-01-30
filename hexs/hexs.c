df6070e5e1dd6cd21c1166d23c5476d18673f1c3
karahiyo <a.ryuklnm@gmail.com>
2013-01-30 09:04:23 +0900

    Hexagon Search.

--- hexs/hexs.c
+++ hexs/hexs.c
@@ -14,22 +14,22 @@ extern int bmc_ave;
 	       float *vecx
   function   : full search
  ===============================================================*/
-int diamondSearch(float *premap, float *crtmap, float *vecy, float *vecx)
+int hexagonSearch(float *premap, float *crtmap, float *vecy, float *vecx)
 {
   double sum, min;
   register int i, j, m, n, x, y, xx, yy;
 
-  struct _diamond_struc {
-      int y;
+  struct _hexagon_struc {
       int x;
+      int y;
   };
 
   /* LDSP */
-  struct _diamond_struc ldsp[9] = 
-            {{0,0},{-2,0},{-1,1},{0,2},{1,1},{2,0},{1,-1},{0,-2},{-1,-1}};
+  struct _hexagon_struc lhsp[7] = 
+            {{0,0},{-2,0},{-1,2},{1,2},{2,0},{1,-2},{-1,-2}};
 
   /* SDSP */
-  struct _diamond_struc sdsp[5] =
+  struct _hexagon_struc shsp[5] =
             {{0,0},{-1,0},{0,1},{1,0},{0,-1}};
 
   /* 探索範囲 */
@@ -37,15 +37,17 @@ int diamondSearch(float *premap, float *crtmap, float *vecy, float *vecx)
     for (x = 0, xx = 0; x < SRC_X_SIZE; x += MB_SIZE, xx++) {
 
       static int now_x, now_y; // 各ステップごとの開始点を記憶
-      double round_min; // diamondサーチの一回の試行の中での最小値の値
+      double round_min; // hexagon-basedサーチの一回の試行の中での最小値の値
       int yet[MB_SIZE][MB_SIZE]; // マクロブロックサイズの配列。各画素位置が探索済かどうかを保持
-      int ldspGreed; // ldsp探索終了マーク
-      int sdspGreed; // sdsp探索終了マーク
-          
+      int lhspGreed; // lhsp探索終了マーク
+      int shspGreed; // shsp探索終了マーク
+      int tmp_vecy=0,tmp_vecx=0;
+      
       min = DBL_MAX;
+      round_min = DBL_MAX;
       now_x = 0, now_y = 0; // 探索開始点(0,0)
-      ldspGreed = 1;
-      int isblind = 0; // 探索空間が行き止まりかフラグ。0なら行き止まり
+      lhspGreed = 1;
+      int isblind; // 探索空間が行き止まりかフラグ。0なら行き止まり
 
       /* マクロブロックサイズの配列をゼロ(=未探索)で初期化 */
       for(n = -SW_SIZE; n <= SW_SIZE; n++)
@@ -54,27 +56,32 @@ int diamondSearch(float *premap, float *crtmap, float *vecy, float *vecx)
       }
 
       
-      while(ldspGreed != 0) {          
-          round_min = DBL_MAX;
-          
-          /* LDSP探索 */
-          for (n = 0; n < 9; n++) {
-              /* 画像端部の例外処理 */
-                if((y + now_y + ldsp[n].y < 0) || 
-                    (x + now_x + ldsp[n].x < 0) ||
-                    (SRC_Y_SIZE < y + now_y + ldsp[n].y + MB_SIZE) ||
-                    (SRC_X_SIZE < x + now_x + ldsp[n].x + MB_SIZE)) continue;
+      while(lhspGreed != 0) {
+          round_min = min;
+          isblind = 0;
+          fprintf(stderr,"NOW(%d,%d) -> ",now_y,now_x);
 
-                if((now_y + ldsp[n].y < -7) ||
-                    (now_x + ldsp[n].x < -7) ||
-                    (now_y + ldsp[n].y > 7) ||
-                    (now_x + ldsp[n].x > 7)) continue;
+          /* LHSP探索 */
+          for (n = 0; n < 7; n++) {
+              /* 画像端部の例外処理 */
+                if((y + now_y + lhsp[n].y < 0) || 
+                    (x + now_x + lhsp[n].x < 0) ||
+                    (SRC_Y_SIZE < y + now_y + lhsp[n].y + MB_SIZE) ||
+                    (SRC_X_SIZE < x + now_x + lhsp[n].x + MB_SIZE)) continue;
+
+                if((now_y + lhsp[n].y < -7) ||
+                    (now_x + lhsp[n].x < -7) ||
+                    (now_y + lhsp[n].y > 7) ||
+                    (now_x + lhsp[n].x > 7)) { 
+                    fprintf(stderr,"\n\t ** out => %d(%d,%d)\n",n,now_y+lhsp[n].y,now_x+lhsp[n].x);
+                    continue;
+                }
 
                 /* 探索済の点か判定 */
-                if (yet[now_y + ldsp[n].y + SW_SIZE][now_x + ldsp[n].x + SW_SIZE]==1)
+                if (yet[now_y + lhsp[n].y + SW_SIZE][now_x + lhsp[n].x + SW_SIZE]==1)
                     continue;
                 else
-                    yet[now_y + ldsp[n].y + SW_SIZE][now_x + ldsp[n].x + SW_SIZE] = 1;
+                    yet[now_y + lhsp[n].y + SW_SIZE][now_x + lhsp[n].x + SW_SIZE] = 1;
 
                 /* 誤差の計算 */
 	            sum = 0.0;
@@ -83,26 +90,30 @@ int diamondSearch(float *premap, float *crtmap, float *vecy, float *vecx)
                 for (j = 0; j < MB_SIZE; j++)
                   for (i = 0; i < MB_SIZE; i++) {
                       sum += fabs(crtmap[(y + j)*SRC_X_SIZE + (x + i)] -
-			            premap[(y + now_y + ldsp[n].y + j)*SRC_X_SIZE + (x + now_x + ldsp[n].x + i)]);
+			            premap[(y + now_y + lhsp[n].y + j)*SRC_X_SIZE + (x + now_x + lhsp[n].x + i)]);
                 }
 
 	            /* 一試行の最小値(round_min)と動きベクトルの更新 */
                 if (sum < round_min) {
                     round_min = sum;
-                    vecy[yy*MB_X_NUM + xx] = ldsp[n].y;
-                    vecx[yy*MB_X_NUM + xx] = ldsp[n].x;
+                    tmp_vecy = lhsp[n].y;
+                    tmp_vecx = lhsp[n].x;
                 }
             } /* }}} for(n)*for(m) */
 
            /* 探索開始点の移動 */
            if(isblind == 1) {
-                now_y += vecy[yy*MB_X_NUM + xx];
-                now_x += vecx[yy*MB_X_NUM + xx];
+                now_y += tmp_vecy;
+                now_x += tmp_vecx;
            } else {
                 vecy[yy*MB_X_NUM + xx] = now_y;
                 vecx[yy*MB_X_NUM + xx] = now_x;
-                ldspGreed = 0;
+                lhspGreed = 0;
+                fprintf(stderr,"::vec(y,x)=(%d,%d)\n",(int)vecy[yy*MB_X_NUM+xx],(int)vecx[yy*MB_X_NUM+xx]);
            }
+
+//            fprintf(stderr,"::vec(y,x)=(%d,%d) \n",vecy[yy*MB_X_NUM+xx],vecx[yy*MB_X_NUM+xx]);
+
 
             /* 終了条件の判定 */
             if (round_min < min )
@@ -110,48 +121,59 @@ int diamondSearch(float *premap, float *crtmap, float *vecy, float *vecx)
             else {
                 vecy[yy*MB_X_NUM + xx] = now_y;
                 vecx[yy*MB_X_NUM + xx] = now_x;
-                ldspGreed = 0;
+                lhspGreed = 0;
+            fprintf(stderr,"::vec(y,x)=(%d,%d)\n",(int)vecy[yy*MB_X_NUM+xx],(int)vecx[yy*MB_X_NUM+xx]);
             }
-    } /* }}} while(ldspGreed) */
+    } /* }}} while(lhspGreed) */
     
   
-  int tmp_vecy=0,tmp_vecx=0;
-  /* SDSP探索 */
+  /* SHSP探索 */
+  tmp_vecy=0,tmp_vecx=0;
+  fprintf(stderr,"\tSHSP(%d,%d) -> ",(int)vecy[yy*MB_X_NUM + xx],(int)vecx[yy*MB_X_NUM + xx]);
   for(n=0; n<5; n++) {
+    isblind = 0; // 行き止まりフラグ OFF
     /* 画像端部の例外処理 */
-    if((y + now_y + sdsp[n].y < 0) || 
-        (x + now_x + sdsp[n].x < 0) ||
-        (SRC_Y_SIZE < y + now_y + sdsp[n].y + MB_SIZE) ||
-        (SRC_X_SIZE < x + now_x + sdsp[n].x + MB_SIZE)) continue;
+    if((y + now_y + shsp[n].y < 0) || 
+        (x + now_x + shsp[n].x < 0) ||
+        (SRC_Y_SIZE < y + now_y + shsp[n].y + MB_SIZE) ||
+        (SRC_X_SIZE < x + now_x + shsp[n].x + MB_SIZE)) continue;
 
-    if((now_y + sdsp[n].y < -7) ||
-        (now_x + sdsp[n].x < -7) ||
-        (now_y + sdsp[n].y > 7) ||
-        (now_x + sdsp[n].x > 7)) continue;
+    if((now_y + shsp[n].y < -7) ||
+        (now_x + shsp[n].x < -7) ||
+        (now_y + shsp[n].y > 7) ||
+        (now_x + shsp[n].x > 7)) { 
+        fprintf(stderr,"\n\t\t ** out => %d(%d,%d)\n",n,now_y+shsp[n].y,now_x+shsp[n].x);
+        continue;
+    }
 
     /* 誤差の計算 */
     sum = 0.0;
     bmcount++; //ブロックマッチング回数カウント
+    isblind = 1; // 行き止まりフラグ ON
     for (j = 0; j < MB_SIZE; j++)
         for (i = 0; i < MB_SIZE; i++) {
             sum += fabs(crtmap[(y + j)*SRC_X_SIZE + (x + i)] -
-                premap[(y + now_y + sdsp[n].y + j)*SRC_X_SIZE + (x + now_x + sdsp[n].x + i)]);
+                premap[(y + now_y + shsp[n].y + j)*SRC_X_SIZE + (x + now_x + shsp[n].x + i)]);
         }
 
     /* 一試行の最小値(round_min)と動きベクトルの更新 */
     if (sum < min) {
         min = sum;
-        tmp_vecy = sdsp[n].y;
-        tmp_vecx = sdsp[n].x;
+        tmp_vecy = shsp[n].y;
+        tmp_vecx = shsp[n].x;
     }
-  } /* }}} for(sdsp) */
+  } /* }}} for(shsp) */
 
-    /* 探索開始点の移動 */
-    vecy[yy*MB_X_NUM + xx] += tmp_vecy;
-    vecx[yy*MB_X_NUM + xx] += tmp_vecx;
+    /* 動きベクトルを更新 */
+    if(isblind == 1) {
+        vecy[yy*MB_X_NUM + xx] += tmp_vecy;
+        vecx[yy*MB_X_NUM + xx] += tmp_vecx;
+    }
+    fprintf(stderr,"\t::vec(y,x)=(%d,%d)\n",(int)vecy[yy*MB_X_NUM+xx],(int)vecx[yy*MB_X_NUM+xx]);
+    
 
   } /* }}} for(SRC_Y_SIZE)*for(SRC_X_SIZE) */
-  fprintf(stderr,"min=%f \n",min);
+  fprintf(stderr,"\n********************************* \nmin=%f \n",min);
   return 0;
 }
 
